bb34bb25bfd94f081c70f91f356009be3b7e872a
Raymond Feng <rf.raymondfeng@gmail.com>
2020-06-17 17:12:25 +1200

    [Master] Cleaned up setup

--- sprocket-server/app.js
+++ sprocket-server/app.js
@@ -3,20 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var app = express();
 
 // view engine setup
 app.set('view engine', 'pug')
 
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '../sprocket-client/build')))
 
 // routes
 app.use('/', require('./routes/index.js'));
--- sprocket-server/routes/index.js
+++ sprocket-server/routes/index.js
@@ -1,23 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({
-    title: 'server',
-    content: `
-      <h1>Express</h1>
-      <p>Welcome to sprocket backend.</h1>
-    `
-  });
+  res.sendFile(path.join(__dirname, '../sprocket-client/build', 'index.html'))
 });
 
 router.get('/api', function(req, res, next) {
   res.json({
-    title: 'server-api',
+    title: 'sprocket-api',
     content: `
-      <h1>Spprax-Server-API</h1>
-      <p>Welcome to sprocket backend.</h1>
+      <h1>Spprax Server API</h1>
+      <p>Welcome to sprocket backend with API access.</p>
     `
   });
 });
